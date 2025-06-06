b3cdc9f63731b1863e532fe3fb00eda4ae783b43
jassilearg <jassilearg@gmail.com>
2025-06-05 21:20:25 -0300

    adicionar caso de teste para usuário não autorizado criar produto

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,6 +1,10 @@
 describe('clientes', () => {
-    it('deve criar um produto', () => {
+    it('deve permitir usuário administrador criar um produto', () => {
         cy.criarProduto();
+    });
+
+    it('não deve permitir usuário sem permissão de administrador criar um produto', () => {
+        cy.criarProdutoSemPermissaoAdm();
     });
 
 });
--- cypress/support/products/commands.js
+++ cypress/support/products/commands.js
@@ -34,3 +34,41 @@ Cypress.Commands.add('criarProduto', () => {
       });
     });
 });
+
+Cypress.Commands.add('criarProdutoSemPermissaoAdm', () => {
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.env('API_URL')}/auth/signIn`,
+    body: {
+      email: "testes@testes.com",
+      password: "Testes@2025"
+    }
+  }).then((response) => {
+    const token = response.body.accessToken;
+
+    cy.request({
+      method: 'POST',
+      failOnStatusCode: false,
+      url: `${Cypress.env('API_URL')}/products`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body: {
+        "name": "Monster No Sugar",
+        "description": "Monster Energy Drink no sugar",
+        "price": 9,
+        "stock": 5,
+        "Tags": [
+          {
+            "label": "no sugar",
+            "value": "noSugar"
+          }
+        ]
+      }
+    }).then((res) => {
+      cy.log('Produto criado:', JSON.stringify(res.body));
+      expect(res.status).to.eq(403);
+      expect(res.body).to.have.property('error').to.eq("Forbidden");
+    });
+  });
+});
