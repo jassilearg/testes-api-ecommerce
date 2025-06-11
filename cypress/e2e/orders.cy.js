a2db5b999204ceac58229f16a91756c82bc9099a
jassilearg <jassilearg@gmail.com>
2025-06-10 21:44:25 -0300

    caso de teste de criação de ordem com tipos de dados incorretos

--- cypress/e2e/orders.cy.js
+++ cypress/e2e/orders.cy.js
@@ -6,4 +6,8 @@ describe('orders', () => {
     it('não deve permitir usuário criar uma ordem com pedido de id inválido', () => {
         cy.criarOrdemIdInvalido();
     });
+
+    it('não deve permitir criar uma ordem com campos com tipos de dados incorretos', () => {
+        cy.criarProdutoComTiposDeDadosIncorretos();
+    });
 });
